5803d737224fd47aff27c79bd207d62fedead9ca
zhao tao <zhaotaoworkspace@gmail.com>
2023-07-26 09:19:06 +0800

    again update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-flight-pods',
+  base: '/vue-project/',
   plugins: [
     vue(),
   ],
